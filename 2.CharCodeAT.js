a46f3b6fe2b22d40d75941ebc274581fb326db27
musaibdeveloper <syedmusaib428@gmail.com>
2023-08-15 11:14:17 +0000

    CharcodeAt()

---
+++ 2.CharCodeAT.js
@@ -0,0 +1,12 @@
+let str = "Musaib";
+
+let store = str.charCodeAt(0);
+
+console.log(store); // 77
+
+// Here in the above code the index value is given and the return value 
+// is the ascii table value. 
+
+// Link : https://alpharithms.s3.amazonaws.com/assets/img/ascii-chart/ascii-table-alpharithms-scaled.jpg
+
+// 0 is at M and M ascii value is  77 
