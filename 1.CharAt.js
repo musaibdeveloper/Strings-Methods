316b6caa41e7755765607e51cf15472bf6f540f8
musaibdeveloper <syedmusaib428@gmail.com>
2023-08-15 10:43:58 +0000

    CharAT is updated

---
+++ 1.CharAt.js
@@ -0,0 +1,12 @@
+// It gives the value by taking the index.
+
+// In string spaces also has value and comma also has value.
+// This entire string is based on ascii Table. 
+
+
+
+let str = "12345";
+
+let store = str.charAt(3);
+
+console.log(store);
